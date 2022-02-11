97a32bd1b1e9f0d252abd1cd6e6aa4030ec9c585
fmau <florence@maurice-web.de>
2022-02-11 16:47:24 +0100

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./input/css");
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  return {
+    dir: {
+      input: "input",
+      output: "public",
+    },
+  };
+};
